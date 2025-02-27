72055e1dc86be641d2c934ace75e94f907c44e32
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-02-27 16:16:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+    origin:["*"],
+    methods:["POST","GET"],
+    credentials:true
+    }
+)); 
 app.use(express.json());
 
 // Function to get latitude and longitude from a city name
@@ -72,5 +78,8 @@ app.post("/calculate-distance", async (req, res) => {
   }
 });
 
+app.get("/",(req,res)=>{
+    res.json("Hm");
+})
 const PORT = 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
